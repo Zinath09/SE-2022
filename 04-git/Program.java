d23813adf26f86826fd9b0963b00ffd9d6d9654d
Zinath09 <zuzabu@o2.pl>
2022-03-26 23:46:01 +0100

    staging etc.

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,7 +1,12 @@
 public class Program {
     public static void main(String[] args) {
+        // Comment
+        System.out.println("1. change");
 
         System.out.println("Hello!");
+        System.out.println("2. change");
+
         System.out.println("Hello for 3. task!");
+        System.out.println("3. change");
     }
 }
