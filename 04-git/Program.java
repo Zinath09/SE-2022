18016c57d745d5586f71c409d30c5f843bb936fa
Zinath09 <zuzabu@o2.pl>
2022-03-27 11:03:14 +0200

    preparing java again

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,12 +1,10 @@
 public class Program {
     public static void main(String[] args) {
-        // Comment
-        System.out.println("1. change");
-
+        // Another comment
+        System.out.println("--1!");
         System.out.println("Hello!");
-        System.out.println("2. change");
-
+        System.out.println("--2!");
         System.out.println("Hello for 3. task!");
-        System.out.println("3. change");
+        System.out.println("--3!");
     }
 }
